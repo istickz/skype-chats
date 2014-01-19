af6a079de4487a34bb71fa9b7ff9eac8ab440cb7
Temur Fatkulin <temur.fatkulin@gmail.com>
2014-01-19 18:24:59 +0600

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 SkypeChats::Application.routes.draw do
+
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}, path_names: {sign_in: "login", sign_out: "logout"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
