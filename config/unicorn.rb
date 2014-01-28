a6ff1b4d5a03e570c2af341b478ef4de638d1529
Temur Fatkulin <temur.fatkulin@gmail.com>
2014-01-29 00:37:17 +0600

    add unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,32 @@
+ENV['RAILS_ENV'] = 'production'
+worker_processes 1  # количество воркер процессов, я люблю делать их по кол-ву ядер например
+preload_app true
+user('skype-chats','skype-chats')  # здесь указываем пользователя и группу его
+timeout 30 # таймаут работы приложения
+
+@app = "/home/skype-chats/skype-chats.com/current"  # путь к нашему размещенному приложению, разумеется project_name меняем на ваш везде
+@shared = "/home/skype-chats/skype-chats.com/shared"  # путь к shared папке
+
+listen "#{@shared}/tmp/unicorn.socket"  # путь где будет лежать открытый Unicorn
+working_directory "#{@app}"
+pid "#{@shared}/tmp/unicorn.pid"  # ключ инстанса нашего запущенного сервера
+stderr_path "#{@shared}/log/unicorn.stderr.log"  # пути к логам unicorn'а
+stdout_path "#{@shared}/log/unicorn.stdout.log"
+
+GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  Rails.cache.reset if Rails.cache.respond_to? :reset
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
