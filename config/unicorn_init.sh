7380e806c8dba6baea952f0a6440979d243beb38
Temur Fatkulin <temur.fatkulin@gmail.com>
2014-02-03 03:11:29 +0600

    update configuration files

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -15,7 +15,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/skype-chats.com/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=username
+AS_USER=skype-chats
 set -u
 
 OLD_PIN="$PID.oldbin"
