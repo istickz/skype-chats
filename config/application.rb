18b06fdac6f8fb3361cc97eb1c7c7c4af8267d0b
Temur Fatkulin <temur.fatkulin@gmail.com>
2014-02-04 00:10:09 +0600

    fix deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ Bundler.require(:default, Rails.env)
 
 module SkypeChats
   class Application < Rails::Application
+    config.i18n.enforce_available_locales = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,8 +23,9 @@ module SkypeChats
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
+
 
   end
 end
